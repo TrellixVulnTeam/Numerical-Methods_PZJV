07f97b3f45f30a3ab0cbbf768d58e45075c47e76
mradrian <mr.podkalicki.adrian94@gmail.com>
2019-08-13 23:05:09 +0200

    tasks

---
+++ lab_1/tasks.py
@@ -0,0 +1,7 @@
+#TASKS (4p)
+#1 calculate & print the value of function y = 2x^2 + 2x + 2 for x=[56, 57, ... 100] (0.5p)
+#2 ask the user for a number and print its factorial (1p)
+#3 write a function which takes an array of numbers as an input and finds the lowest value. Return the index of that element and its value (1p)
+#4 looking at lab1-input and lab1-plot files create your own python script that takes a number and returns any chart of a given length.
+#the length of a chart is the input to your script. The output is a plot (it doesn't matter if it's a y=x or y=e^x+2x or y=|x| function, use your imagination)
+#test your solution properly. Look how it behaves given different input values. (1p)
